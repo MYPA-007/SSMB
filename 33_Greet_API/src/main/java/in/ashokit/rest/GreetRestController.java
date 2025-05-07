a2452a1b97dfe02056930840b287f03a1561cf54
sai <saitejamypa007@gmail.com>
2025-05-07 09:59:05 +0530

    hi X34

--- 33_Greet_API/src/main/java/in/ashokit/rest/GreetRestController.java
+++ 33_Greet_API/src/main/java/in/ashokit/rest/GreetRestController.java
@@ -3,27 +3,17 @@ package in.ashokit.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import in.ashokit.client.WelcomeApiClient;
-
 @RestController
 public class GreetRestController {
-
 	@Autowired
 	private WelcomeApiClient apiClient;
-
 	@GetMapping("/greet")
 	public String getGreetMsg() {
-
 		String greetResponse = "Good Morning";
-
-<<<<<<< HEAD
 		String yy="Hey Bro HOw Are Bro";
-=======
-String yy="Hey Bro HOw Are U";
->>>>>>> 12e82dd01920a46b6a987343d9cd846bd6f1eadb
+		String yy="Hey Bro HOw Are U";
 		String welcomeResponse = apiClient.invokeWelcomeApi();
-
 		return greetResponse + ", " + welcomeResponse;
 	}
 }
